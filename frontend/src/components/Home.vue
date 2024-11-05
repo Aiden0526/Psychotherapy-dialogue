ebb8d38c6bb96b5a7726ce8e2c6d71ebf481e9f8
Aiden <aiden@AidendeMacBook-Air.local>
2024-11-05 14:07:14 +0800

    feat(frontend-home): Add home page with psychologist selection

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Select Your Psychologist</h1>
+    <button @click="selectPsychologist(1)">Sigmund Freud</button>
+    <button @click="selectPsychologist(2)">Carl Jung</button>
+    <button @click="selectPsychologist(3)">Alfred Adler</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    selectPsychologist(id) {
+      // Navigate to the introduction page for the selected psychologist
+      this.$router.push(`/psychologist/${id}/intro`);
+    }
+  }
+};
+</script>
